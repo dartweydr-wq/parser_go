c42e4ffb4e050e844de1ed127ff3df15ea2bec37
Aleksey <badproper98@gmail.com>
2023-05-12 11:00:54 +0300

    добавление url из флага --url=ссылка

--- main.go
+++ main.go
@@ -2,19 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"github.com/gocolly/colly"
 )
 
 type Book struct {
-	Name           string
-	Desc           string
-	Picture        string
-	LinkOnOzon     string
-	Characteristic BookCharacteristic
-}
-
-type BookCharacteristic struct {
+	Name         string
+	Desc         string
+	Picture      string
+	LinkOnOzon   string
 	PagesCount   string
 	Cover        string
 	Age          string
@@ -29,10 +26,9 @@ type BookCharacteristic struct {
 func main() {
 
 	b := &Book{}
-
-	c := colly.NewCollector(
-		colly.AllowedDomains("rosman.ru"),
-	)
+	flagName := flag.String("url", "", "")
+	c := colly.NewCollector()
+	flag.Parse()
 
 	c.OnHTML(".item-line-first", func(e *colly.HTMLElement) {
 		b.Name = e.ChildText("h1")
@@ -46,24 +42,23 @@ func main() {
 	})
 	c.OnHTML(".tab-content #chars", func(e *colly.HTMLElement) {
 		e.ForEach("dl", func(i int, el *colly.HTMLElement) {
-			b.Characteristic.PagesCount = el.ChildText("dd:nth-child(2)")
-			b.Characteristic.Cover = el.ChildText("dd:nth-child(6)")
-			b.Characteristic.Age = el.ChildText("dd:nth-child(10)")
-			b.Characteristic.Series = el.ChildText("dd:nth-child(14) a")
-			b.Characteristic.Types = el.ChildText("dd:nth-child(18)")
-			b.Characteristic.Size = el.ChildText("dd:nth-child(22)")
-			b.Characteristic.VendorCode = el.ChildText("dd:nth-child(26)")
-			b.Characteristic.Weight = el.ChildText("dd:nth-child(30)")
-			b.Characteristic.Illustration = el.ChildText("dd:nth-child(34)")
+			b.PagesCount = el.ChildText("dd:nth-child(2)")
+			b.Cover = el.ChildText("dd:nth-child(6)")
+			b.Age = el.ChildText("dd:nth-child(10)")
+			b.Series = el.ChildText("dd:nth-child(14) a")
+			b.Types = el.ChildText("dd:nth-child(18)")
+			b.Size = el.ChildText("dd:nth-child(22)")
+			b.VendorCode = el.ChildText("dd:nth-child(26)")
+			b.Weight = el.ChildText("dd:nth-child(30)")
+			b.Illustration = el.ChildText("dd:nth-child(34)")
 		})
 	})
-	c.Visit("https://rosman.ru/catalog/item/luchshie-skazki-na-noch/")
+	c.Visit(*flagName)
 
 	data, err := json.Marshal(b)
 	if err != nil {
 		fmt.Println("JSON marshaling failed: %s", err)
 	}
 
-	//fmt.Println(b)
 	fmt.Println(string(data))
 }
