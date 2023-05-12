8c5eaf8a023d75efd246c8fc273bfcb889eb28e6
Aleksey <badproper98@gmail.com>
2023-05-12 10:29:30 +0300

    update

--- main.go
+++ main.go
@@ -1,61 +1,69 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gocolly/colly"
 )
 
-type books struct {
-	nameBook        string
-	descBook        string
-	pictureBook     string
-	linkOnOzonBook  string
-	characteristics []interface{}
+type Book struct {
+	Name           string
+	Desc           string
+	Picture        string
+	LinkOnOzon     string
+	Characteristic BookCharacteristic
 }
 
-type characteristicsBooks struct {
-	countPagesBook int
-	coverBook      string
-	ageBook        string
-	seriesBook     string
-	typeBook       string
-	sizeBook       string
-	vendorCode     string
-	weight         string
-	illustration   string
+type BookCharacteristic struct {
+	PagesCount   string
+	Cover        string
+	Age          string
+	Series       string
+	Types        string
+	Size         string
+	VendorCode   string
+	Weight       string
+	Illustration string
 }
 
 func main() {
 
-	b := books{}
+	b := &Book{}
 
 	c := colly.NewCollector(
 		colly.AllowedDomains("rosman.ru"),
 	)
 
-	// Find and print all links
 	c.OnHTML(".item-line-first", func(e *colly.HTMLElement) {
-		b.nameBook = e.ChildText("h1")
+		b.Name = e.ChildText("h1")
 	})
 	c.OnHTML(".item-line-left", func(e *colly.HTMLElement) {
-		b.descBook = e.ChildText(".detailtext p")
-		b.pictureBook = e.ChildAttr(".item-pic img", "src")
+		b.Desc = e.ChildText(".detailtext p")
+		b.Picture = e.ChildAttr(".item-pic img", "src")
 	})
 	c.OnHTML(".marketplace-block", func(e *colly.HTMLElement) {
-		b.linkOnOzonBook = e.ChildAttr(".ozon", "href")
+		b.LinkOnOzon = e.ChildAttr(".ozon", "href")
 	})
 	c.OnHTML(".tab-content #chars", func(e *colly.HTMLElement) {
-		e.ForEach("dd", func(_ int, el *colly.HTMLElement) {
-			b.characteristics = append(b.characteristics, el.Text)
+		e.ForEach("dl", func(i int, el *colly.HTMLElement) {
+			b.Characteristic.PagesCount = el.ChildText("dd:nth-child(2)")
+			b.Characteristic.Cover = el.ChildText("dd:nth-child(6)")
+			b.Characteristic.Age = el.ChildText("dd:nth-child(10)")
+			b.Characteristic.Series = el.ChildText("dd:nth-child(14) a")
+			b.Characteristic.Types = el.ChildText("dd:nth-child(18)")
+			b.Characteristic.Size = el.ChildText("dd:nth-child(22)")
+			b.Characteristic.VendorCode = el.ChildText("dd:nth-child(26)")
+			b.Characteristic.Weight = el.ChildText("dd:nth-child(30)")
+			b.Characteristic.Illustration = el.ChildText("dd:nth-child(34)")
 		})
 	})
 	c.Visit("https://rosman.ru/catalog/item/luchshie-skazki-na-noch/")
 
-	/*data, err := json.Marshal(&b)
+	data, err := json.Marshal(b)
 	if err != nil {
 		fmt.Println("JSON marshaling failed: %s", err)
-	}*/
+	}
 
-	fmt.Println(b)
-	//fmt.Println(data)
+	//fmt.Println(b)
+	fmt.Println(string(data))
 }
